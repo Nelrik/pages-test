e4769727afa7990f50e21d1b68fe2f1465aa8aed
Nelrik <addevel@outlook.com>
2023-10-10 17:43:21 -0600

    Test on pages

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,11 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('pages-test');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('pages-test app is running!');
-  });
 });
